899aa44b4a0798c62468458a361f60c51963960a
DelioCoder <davideveriwhere@gmail.com>
2025-09-19 16:06:59 -0500

    new attributes to parse

--- src/lambdas/parser/dto/metadata-dto.ts
+++ src/lambdas/parser/dto/metadata-dto.ts
@@ -1,35 +1,79 @@
 import { Type } from "class-transformer";
-import { ArrayMinSize, IsArray, IsString, ValidateNested } from "class-validator";
+import {
+  ArrayMinSize,
+  IsArray,
+  IsOptional,
+  IsString,
+  ValidateNested,
+  IsIn
+} from "class-validator";
 
 export class MetadataDto {
+  @IsString()
+  app_cmdb: string;
 
-    @IsString()
-    plataforma: string;
+  @IsString()
+  module_cmdb: string;
 
-    @IsArray()
-    @ArrayMinSize(1)
-    @ValidateNested({ each: true })
-    @Type(() => MicroFrontend)
-    microfrontends: MicroFrontend[];
+  @IsString()
+  categoria: string;
+
+  @IsString()
+  @IsIn(['monorepo', 'multirepo', 'multirepo-conexiones', 'host-multi-auth'])
+  estructura: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @ValidateNested({ each: true })
+  @Type(() => MicroFrontend)
+  instancias: MicroFrontend[];
 }
 
 export class MicroFrontend {
+  @IsString()
+  codigo: string;
 
-    @IsString()
-    id: string;
+  @IsString()
+  nombre: string;
 
-    @IsString()
-    nombre: string;
+  @IsString()
+  tipo: string;
 
-    @IsString()
-    tipo: string;
+  @IsString()
+  version: string;
 
-    @IsString()
-    version: string;
+  @IsString()
+  repositorio: string;
 
-    @IsString()
-    repositorio: string;
+  @IsString()
+  @IsOptional()
+  path?: string;
 
-    @IsString()
-    estado: string
+  @IsString()
+  estado: string;
+
+  @IsArray()
+  @IsString({ each: true })
+  funcionalidades: string[];
+
+  // conexiones cruzadas
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => Conexion)
+  conexiones?: Conexion[];
+
+  // múltiples auth
+  @IsOptional()
+  @IsArray()
+  @IsString({ each: true })
+  authProviders?: string[];
+}
+
+export class Conexion {
+  @IsString()
+  plataforma: string;
+
+  @IsString()
+  mfe_id: string;
 }

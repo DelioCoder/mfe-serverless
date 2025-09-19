85dcc73a8607b43c51fbd77d6b329a683b02d6e8
DelioCoder <davideveriwhere@gmail.com>
2025-09-19 16:08:12 -0500

    retrieving all relations

--- src/lambdas/consultas-relaciones/index.ts
+++ src/lambdas/consultas-relaciones/index.ts
@@ -1,23 +1,33 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { getRelacionesByMfeId } from "./resources/dynamodb";
+import { getMfesGroupedByPlataforma, getRelacionesByMfeId } from "./resources/dynamodb";
 
 const RELACIONES_TABLA = process.env.RELACIONES_TABLA!;
 
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
 
-        if(!event.pathParameters?.id){
-            return { statusCode: 400, body: JSON.stringify('No se proporciono el id del microfrontend') }
+        if (event.resource === "/mfes/{id}") {
+            if (!event.pathParameters?.id) {
+                return { statusCode: 400, body: JSON.stringify('No se proporciono el id del microfrontend') }
+            }
+
+            const platformName = decodeURIComponent(event.pathParameters.id);
+
+            const result = await getRelacionesByMfeId(RELACIONES_TABLA, platformName);
+
+            return {
+                statusCode: 200,
+                body: JSON.stringify(result)
+            }
         }
 
-        const platformName = decodeURIComponent(event.pathParameters.id);
-
-        const result = await getRelacionesByMfeId(RELACIONES_TABLA, platformName);
+        const result = await getMfesGroupedByPlataforma(RELACIONES_TABLA);
 
         return {
             statusCode: 200,
             body: JSON.stringify(result)
         }
+
     } catch (error) {
         return { statusCode: 500, body: JSON.stringify(error) }
     }
